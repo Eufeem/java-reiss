c02842c866c2d46db3bd9a822aa14d2c93df6a98
Eufeem <eufeemgr@gmail.com>
2023-04-09 01:21:51 -0600

    Initial commit

---
+++ src/main/java/com/feem/reiss/ReissApplication.java
@@ -0,0 +1,13 @@
+package com.feem.reiss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReissApplication.class, args);
+	}
+
+}
