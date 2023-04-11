cc5cc858f12d12e1218621278c1d5e7282fe8254
Eufeem <eufeemgr@gmail.com>
2023-04-10 23:08:02 -0600

    Export Excel

--- src/main/java/com/feem/reiss/controller/UserController.java
+++ src/main/java/com/feem/reiss/controller/UserController.java
@@ -2,11 +2,17 @@ package com.feem.reiss.controller;
 
 import com.feem.reiss.model.User;
 import com.feem.reiss.service.UserService;
+import com.feem.reiss.utils.UserExcelExporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -43,5 +49,22 @@ public class UserController {
         System.out.println(user.getName());
         return new ResponseEntity<List<User>>(userService.findByName(user.getName()), HttpStatus.OK);
     }
+
+    @GetMapping("/xlxs")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        List<User> listUsers = userService.getAll();
+        UserExcelExporter excelExporter = new UserExcelExporter(listUsers);
+
+        excelExporter.export(response);
+    }
+
 
 }
