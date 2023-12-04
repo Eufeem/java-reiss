8792a36f57d58d290bba5f7022b9c3300215aaec
Eufeem <eufeemgr@gmail.com>
2023-12-03 23:01:57 -0600

    New schema changes

--- src/main/java/com/feem/reiss/model/User.java
+++ src/main/java/com/feem/reiss/model/User.java
@@ -20,9 +20,14 @@ public class User {
     @Column(name = "LAST_NAME")
     private String lastName;
 
+    @Column(name = "PASSWORD")
+    private String password;
+
     @Column(name = "EMAIL")
     private String email;
 
+    @Column(name = "STATUS")
+    private String status;
 
     public Integer getIdUser() {
         return idUser;
@@ -56,11 +61,27 @@ public class User {
         this.lastName = lastName;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
