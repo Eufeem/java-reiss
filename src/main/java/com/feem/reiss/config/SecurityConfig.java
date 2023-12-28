f979232037f23fffdaa5d27e6f200e44213d6539
Eufeem <eufeemgr@gmail.com>
2023-12-28 11:40:53 -0600

    JWT Auth

--- src/main/java/com/feem/reiss/config/SecurityConfig.java
+++ src/main/java/com/feem/reiss/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.feem.reiss.config;
 
+import com.feem.reiss.jwt.JWTAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private AuthenticationProvider authProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,8 +27,11 @@ public class SecurityConfig {
         http.authorizeHttpRequests(authRequest -> authRequest
                 .antMatchers("/auth/**", "/**").permitAll()
                 .anyRequest().authenticated()
-        ).formLogin();
+        );
 
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authenticationProvider(authProvider);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
--- src/main/java/com/feem/reiss/service/impl/AuthServiceImpl.java
+++ src/main/java/com/feem/reiss/service/impl/AuthServiceImpl.java
@@ -1,15 +1,36 @@
 package com.feem.reiss.service.impl;
 
+import com.feem.reiss.dao.UserDao;
+import com.feem.reiss.jwt.JwtService;
 import com.feem.reiss.model.AuthResponse;
 import com.feem.reiss.model.LoginRequest;
 import com.feem.reiss.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Override
-    public AuthResponse login(LoginRequest loginRequest) {
-        return null;
+    public AuthResponse login(LoginRequest request) {
+        // Use password Bcrypt -- only coment for this example
+        // authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userDao.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setToken(token);
+        return authResponse;
     }
 }
